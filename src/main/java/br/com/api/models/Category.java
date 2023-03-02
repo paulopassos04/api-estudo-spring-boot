6090d3dd1d5091df9f18b5c384944b7636fc2a42
Paulo César Passos Pereira <paulo.passos1@gmail.com>
2023-03-02 10:27:09 -0300

    Many-to-many association with JoinTable

--- src/main/java/br/com/api/models/Category.java
+++ src/main/java/br/com/api/models/Category.java
@@ -1,5 +1,6 @@
 package br.com.api.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import java.io.Serializable;
 import java.util.HashSet;
@@ -15,7 +16,8 @@ public class Category implements Serializable {
     private Long id;
     private String name;
 
-    @Transient
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
 
     public Category(){}
--- src/main/java/br/com/api/models/Product.java
+++ src/main/java/br/com/api/models/Product.java
@@ -18,7 +18,8 @@ public class Product implements Serializable {
     private Double price;
     private String imgUrl;
 
-    @Transient
+    @ManyToMany
+    @JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
 
     public Product(){}
@@ -71,8 +72,8 @@ public class Product implements Serializable {
         this.imgUrl = imgUrl;
     }
 
-    public void setCategories(Set<Category> categories) {
-        this.categories = categories;
+    public Set<Category> getCategories() {
+        return categories;
     }
 
     @Override
