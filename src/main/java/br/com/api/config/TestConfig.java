4526f7f4f385d6d5987c0647d282d2b81d3cc853
Paulo César Passos Pereira <paulo.passos1@gmail.com>
2023-03-02 08:40:02 -0300

    Category

--- src/main/java/br/com/api/config/TestConfig.java
+++ src/main/java/br/com/api/config/TestConfig.java
@@ -1,8 +1,10 @@
 package br.com.api.config;
 
 import br.com.api.enums.OrderStatus;
+import br.com.api.models.Category;
 import br.com.api.models.Order;
 import br.com.api.models.User;
+import br.com.api.repositories.CategoryRepository;
 import br.com.api.repositories.OrderRepository;
 import br.com.api.repositories.UserRepositories;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +25,9 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     public void run(String... args) throws Exception {
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
@@ -32,9 +37,14 @@ public class TestConfig implements CommandLineRunner {
         Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
 
         userRepositories.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 
     }
 }
---
+++ src/main/java/br/com/api/controllers/CategoryController.java
@@ -0,0 +1,32 @@
+package br.com.api.controllers;
+
+import br.com.api.models.Category;
+import br.com.api.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping
+    public ResponseEntity<List<Category>> findAll(){
+        List<Category> list = categoryService.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Category> findById(@PathVariable Long id){
+        Category obj = categoryService.findById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(obj);
+    }
+}
